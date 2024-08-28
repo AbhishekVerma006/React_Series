37a81d8b655eabe4e293a0cda729e193b9719312
AbhishekVerma006 <abhishekverma84951@gmail.com>
2024-08-28 18:23:42 +0530

    run code by three -> 1. React 2. JSX 3.createElement

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export const App = ()=>{
-  return <h1>hello</h1>
-};
+
+
+// export const App = ()=>{
+//   return <h1>hello This is a Abhishek</h1>
+// };
+
+
+// import React from "react"
+// export const App = ()=>{
+//   return React.createElement('h1',null,"hello this is a Abhishek");
+// };
+
+
+// import {jsx as _jsx} from "react/jsx-runtime"
+// export const App = ()=>{
+//   return _jsx( 'h1',{name:null, children:"hello"});
+// };
